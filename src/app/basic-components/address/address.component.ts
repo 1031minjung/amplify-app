8c7710ff8f6593b47d44e0c87d2d4f9a5b09634f
1031minjung <1031minjung@gmail.com>
2022-09-09 22:09:31 -0400

    updated address component with form

--- src/app/basic-components/address/address.component.ts
+++ src/app/basic-components/address/address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
@@ -6,25 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.scss'],
 })
 export class AddressComponent implements OnInit {
-  constructor() {}
-  newAddress: any;
-  ngOnInit(): void {}
-  test="testttt"
-
-  formattedAddress = '';
-  public AddressChange(address: any) {
-    //setting address from API to local variable
-    this.formattedAddress = address.formatted_address;
-    console.log(address);
-    this.newAddress = address;
-    console.log(this.newAddress);
-    console.log(this.newAddress.address_components);
-    console.log(this.newAddress.address_components[0].long_name);
-  }
-
-  public AddressChange2(address: any) {
-    console.log("2222")
-  }
-
+  public addressForm: FormGroup = new FormGroup({
+    streetAddress: new FormControl(''),
+    unitNumber: new FormControl(''),
+    city: new FormControl(''),
+    province: new FormControl(''),
+    country: new FormControl(''),
+    postalCode: new FormControl('')
+  });
+  selectedAddress: any;
   
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.selectedAddress = {
+      streetAddress: '',
+      city: '',
+      province: '',
+      country: '',
+      postalCode: ''
+    };
+  }
+  
+  public AddressChange(address: any) {
+    this.selectedAddress = {
+      streetAddress: address.formatted_address.split(",")[0],
+      city: address.address_components[2].long_name,
+      province: address.address_components[5].short_name,
+      country: address.address_components[6].long_name,
+      postalCode: address.address_components[7].short_name
+    };
+    console.log(this.selectedAddress)
+  }
+
+  public onAddressSave() {
+    console.log("onAddressSave")
+  }
 }
